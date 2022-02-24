a326c62e167b98b22fc1c74055fb5f365b77e909
Web-Dev-Jr <samkautz@yahoo.com>
2022-02-23 22:57:35 -0600

    Move favicon to root in attempt to resolve Heroku error

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='images/favicon.png')
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='images/favicon.png')
 
 
 @app.route('/')
