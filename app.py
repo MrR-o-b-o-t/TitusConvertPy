4bf5222ca40959845c55154a7e655ee5726ee057
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2023-06-09 11:25:49 -0500

    Update formatting for converted Excel doc in app.py

--- app.py
+++ app.py
@@ -1,4 +1,33 @@
-from operator import index
+# from operator import index
+# from flask import Flask, render_template, request, send_file
+# import pandas as pd
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def index():
+#     return render_template('updated_index.html')
+
+
+# @app.route('/convert', methods=['POST'])
+# def convert():
+#     file = request.files['file']
+#     if not file:
+#         return "No file uploaded"
+
+#     try:
+#         df = pd.read_csv(file)
+#         output_file = 'output.xlsx'
+#         df.to_excel(output_file, index=False)
+#         return send_file(output_file, as_attachment=True)
+#     except Exception as e:
+#         return str(e)
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 from flask import Flask, render_template, request, send_file
 import pandas as pd
 
@@ -7,7 +36,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('updated_index.html')
+    return render_template('index.html')
 
 
 @app.route('/convert', methods=['POST'])
@@ -18,8 +47,14 @@ def convert():
 
     try:
         df = pd.read_csv(file)
+
+        df['Full Name'] = df['First Name'] + ' ' + df['Last Name']
+
+        df.drop(['First Name', 'Last Name', 'Headline'], axis=1, inplace=True)
+        df.set_index('Full Name', inplace=True)
+        df.dropna(axis=1, how='all', inplace=True)
         output_file = 'output.xlsx'
-        df.to_excel(output_file, index=False)
+        df.to_excel(output_file)
         return send_file(output_file, as_attachment=True)
     except Exception as e:
         return str(e)
